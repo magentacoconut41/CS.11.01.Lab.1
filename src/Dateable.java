ca4da94ac0657198e2564c77d0b6403d09f07292
AURELIUS WONG <aurelius.wong422@gmail.com>
2024-08-23 12:45:25 +0800

    lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int userAge = scanner.nextInt();
+        int dateableAge = 7 + userAge / 2;
+        System.out.println(userAge + " year olds should date someone at least " + dateableAge);    
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int userAge = scanner.nextInt();
+        int pagesRead = 100 - userAge;
+        System.out.println(userAge + " year olds should read at least " + pagesRead + " pages before giving up on a book");    
 
     }
 }
